b2bbc4274000280dbda8d54f75e7fdeeb91c5c2e
MladenGolub <gm210544d@student.etf.bg.ac.rs>
2026-04-28 00:40:29 +0200

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {    
+public:
+
+    vector<vector<int>> res;
+
+    void backtrack(vector<int>& nums, int start) {
+
+        if(start == nums.size()) {
+            this->res.push_back(nums);
+            return;
+        }
+
+        for(int i = start; i < nums.size(); i++) {
+            swap(nums[start], nums[i]);
+            backtrack(nums, start+1);
+            swap(nums[start], nums[i]);
+        }
+
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        backtrack(nums, 0);        
+        return this->res;
+    }
+
+    
+};
