b5493c87d4158fbad6cbab7cf4a14e2934d00652
MladenGolub <gm210544d@student.etf.bg.ac.rs>
2026-04-30 23:31:36 +0200

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+        int pom = nums[0] ^ nums[1];
+        for(int i = 2; i < nums.size(); i++) {
+            pom = pom ^ nums[i];
+        }
+
+        return pom;
+
+    }
+};
