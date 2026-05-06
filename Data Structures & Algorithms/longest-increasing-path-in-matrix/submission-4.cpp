25ed930516f29b3e6730ed5d60cb60ee9ff41f64
MladenGolub <gm210544d@student.etf.bg.ac.rs>
2026-05-06 19:27:56 +0200

    Add: longest-increasing-path-in-matrix - submission-4

---
+++ Data Structures & Algorithms/longest-increasing-path-in-matrix/submission-4.cpp
@@ -0,0 +1,37 @@
+class Solution {
+
+    vector<vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+    vector<vector<int>> dp;
+
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        int rows = matrix.size(); int col = matrix[0].size();
+        dp = vector<vector<int>>(rows, vector<int>(col, -1));
+        int LIP = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < col; c++) {
+                LIP = max(LIP, dfs(matrix, r, c, INT_MIN));
+            }
+        }
+        return LIP;
+
+    }   
+
+    int dfs(vector<vector<int>> matrix, int r, int c, int prevVal) {
+        int ROWS = matrix.size(), COLS = matrix[0].size();
+        if (r < 0 || r >= ROWS || c < 0 ||
+            c >= COLS || matrix[r][c] <= prevVal) {
+            return 0;
+        }
+        if (dp[r][c] != -1) return dp[r][c];
+
+        int res = 1;
+        for (vector<int> d : directions) {
+            res = max(res, 1 + dfs(matrix, r + d[0],
+                               c + d[1], matrix[r][c]));
+        }
+        dp[r][c] = res;
+        return res;
+    }
+};
