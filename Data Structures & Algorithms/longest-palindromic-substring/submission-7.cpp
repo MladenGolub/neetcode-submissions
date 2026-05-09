5276cba91be76f9123f930f0b7816dd8031c9ee6
MladenGolub <gm210544d@student.etf.bg.ac.rs>
2026-05-09 15:12:18 +0200

    Add: longest-palindromic-substring - submission-7

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-7.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+
+        string najduziNeparni = "";
+        string najduziParni = "";
+
+        for(int i = 0; i < s.size(); i++) {
+            string pom = longestPali(i, i, s);
+            if(pom.size() > najduziNeparni.size()) najduziNeparni = pom;
+        }
+
+        for(int i = 0; i < s.size()-1; i++) {
+            string pom = longestPali(i, i+1, s);
+            if(pom.size() > najduziParni.size()) najduziParni = pom;
+        }
+
+        return (najduziNeparni.size() > najduziParni.size()) ? najduziNeparni : najduziParni;
+
+    }
+
+    string longestPali(int i, int j, string s) {
+        string pom = "";
+        while(i >= 0 && j < s.size() && s[i] == s[j]) {
+            i--;
+            j++;
+        }
+
+        return s.substr(i+1, j - i - 1);
+
+    }
+    
+};
