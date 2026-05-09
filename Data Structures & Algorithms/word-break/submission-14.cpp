23522b33b530d4221e99f0c12264c4f2b2870d54
MladenGolub <gm210544d@student.etf.bg.ac.rs>
2026-05-09 17:34:08 +0200

    Add: word-break - submission-14

---
+++ Data Structures & Algorithms/word-break/submission-14.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        
+        vector<bool> dp(s.size()+1, false);
+
+        dp[s.size()] = true;
+
+        for(int i = s.size()-1; i>= 0; i--) {
+            string pom;
+            for(int j = 0; j < wordDict.size(); j++) {
+                if(wordDict[j].size() <= s.size() - i) {
+                    pom = s.substr(i, wordDict[j].size());
+                    if(pom == wordDict[j]) {
+                        dp[i] = dp[i+pom.size()];
+                    }
+                    if(dp[i] == true) break;
+                } 
+            }
+        }
+
+        return dp[0];
+ 
+    }
+};
