430d135b80e96eb1128816a10175d1cc512ef581
MladenGolub <gm210544d@student.etf.bg.ac.rs>
2026-05-09 20:01:57 +0200

    Add: count-number-of-islands - submission-5

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-5.cpp
@@ -0,0 +1,46 @@
+class Solution {
+
+    int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int rows = grid.size();
+        int col = grid[0].size();
+        int islands = 0;
+        for(int r = 0; r < rows; r++) {
+            for(int c = 0; c < col; c++) {
+                if(grid[r][c] == '1') {
+                    bfs(grid, r, c);
+                    islands++;
+                }
+            }
+        }
+        return islands;
+    }
+
+    void bfs(vector<vector<char>>& grid, int r, int c) {
+
+        queue<pair<int,int>> q;
+        grid[r][c] = '0';
+        q.push({r, c});
+
+        while(!q.empty()) {
+            pair<int,int> pom = q.front(); 
+            q.pop();//skidamo iz reda
+            int row = pom.first; 
+            int col = pom.second;
+            for(int i = 0; i < 4; i++) {
+                int nrow = row + directions[i][0];
+                int ncol = col + directions[i][1];
+                if(nrow >= 0 && ncol >= 0 && nrow < grid.size() &&
+                ncol < grid[0].size() && grid[nrow][ncol] == '1') {
+                    q.push({nrow, ncol});
+                    grid[nrow][ncol] = '0';
+                }
+            }
+        }
+
+    }
+
+
+};
