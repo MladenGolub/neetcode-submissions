eb0243e1bafe4a08dec6e344075e552a34daf51f
MladenGolub <gm210544d@student.etf.bg.ac.rs>
2026-05-09 20:41:38 +0200

    Add: course-schedule - submission-1

---
+++ Data Structures & Algorithms/course-schedule/submission-1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+
+    map<int, vector<int>> mapa;
+    set<int> visited;
+
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        for(int i = 0; i < numCourses; i++) {
+            mapa[i] = vector<int>();
+        }
+
+        for(auto& p: prerequisites) {
+            mapa[p[0]].push_back(p[1]);
+        }//za svaki cvor dodajemo koje on ima preduslove za slusanje
+
+
+        for(int i = 0; i< numCourses; i++) {
+            if(!dfs(i)) return false;
+        }
+
+        return true;
+
+    }
+
+    bool dfs(int course) {
+        if(visited.find(course) != visited.end()) return false;
+        if(mapa[course].size() == 0) {
+            return true;
+        }
+
+        visited.insert(course);
+        for(auto& p: mapa[course]) {
+            if(!dfs(p)) return false;
+        }
+        visited.erase(course);//mi prvo ubacimo svaki koji trenutno obilazimo, jer moze doci do petlje ako je njih vise povezano u oba smera, zato pazimo koje smo obisli,
+        //kada zavrsimo taj dfs obilazak onda moramo da izbacimo sve te cvorove koje smo obilazili jer se oni ne nalaze mozda na drugoj putanji na koju sada krecemo
+        mapa[course] = vector<int>();//stavljamo ga na prazan
+        return true;
+    }
+};
