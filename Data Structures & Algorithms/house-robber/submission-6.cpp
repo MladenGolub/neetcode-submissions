909585a61539f3f8cd86ea600d0940b7c9984c4e
MladenGolub <gm210544d@student.etf.bg.ac.rs>
2026-05-09 14:31:55 +0200

    Add: house-robber - submission-6

---
+++ Data Structures & Algorithms/house-robber/submission-6.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        
+        int rob1 = 0;
+        int rob2 = 0;
+
+        for(int i = 0; i < nums.size(); i++) {
+            int tmp = max(rob1 + nums[i], rob2);
+            rob1 = rob2;
+            rob2 = tmp;
+        }
+
+        return rob2;
+
+    }
+};
