4f3cfdd865b53c8bb7d36e8e82e07e093c88d0f7
MladenGolub <gm210544d@student.etf.bg.ac.rs>
2026-05-09 20:45:24 +0200

    Add: course-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/course-schedule-ii/submission-2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+
+    set<int> visited;
+    map<int, vector<int>> mapa;
+    vector<int> redosled;
+
+public:
+    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+        for(int i = 0; i < numCourses; i++) {
+            mapa[i] = vector<int>();
+        }
+
+        for(auto& p: prerequisites) {
+            mapa[p[0]].push_back(p[1]);
+        }//za svaki cvor dodajemo koje on ima preduslove za slusanje
+
+        for(int i = 0; i < numCourses; i++) {
+            if(mapa[i].size() == 0) redosled.push_back(i);
+        }
+
+        for(int i = 0; i< numCourses; i++) {
+            if(!dfs(i)) return vector<int>();
+        }
+
+        return redosled;
+
+    }
+
+    bool dfs(int course) {
+        if(visited.find(course) != visited.end()) return false;//!!!!! NAJBITNIJE U OVOM ZADATKU JE PRACENJE DA LI SE NALAZIMO U PETLJI ZA ODREDJENU POLAZNU TACKU, JER PREKO TOGA ZNAMO DA LI MOZEMO
+        //DA SKLONIMO USLOVLJENOST, TJ DA JE ISPUNIMO
+        if(mapa[course].size() == 0) {
+            return true;
+        }
+
+        visited.insert(course);
+        for(auto& p: mapa[course]) {
+            if(!dfs(p)) return false;
+        }
+        visited.erase(course);//mi prvo ubacimo svaki koji trenutno obilazimo, jer moze doci do petlje ako je njih vise povezano u oba smera, zato pazimo koje smo obisli,
+        //kada zavrsimo taj dfs obilazak onda moramo da izbacimo sve te cvorove koje smo obilazili jer se oni ne nalaze mozda na drugoj putanji na koju sada krecemo
+        mapa[course] = vector<int>();//stavljamo ga na prazan
+        redosled.push_back(course);
+        return true;
+    }
+};
