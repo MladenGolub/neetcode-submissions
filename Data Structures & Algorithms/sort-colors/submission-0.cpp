c0fe5c118fc2fb185256cda49f2c6575677f0741
Mladen Golubovic <gm210544d@student.etf.bg.ac.rs>
2026-08-04 19:08:53 +0200

    Add: sort-colors - submission-0

---
+++ Data Structures & Algorithms/sort-colors/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        vector<int> cnt(3);//imam vektor sa 3 pozicije i racunam za 3 boje i koristim count sort
+        
+        for(int boja: nums) {
+            cnt[boja]++;
+        }
+
+        for(int i = 0, j = 0; i < nums.size() && j<cnt.size(); i++) {
+            if(cnt[j] > 0) {
+                nums[i] = j;
+                cnt[j]--;
+            }else{
+            j++;
+            i--;
+            }
+        }
+        
+
+    }
+};
