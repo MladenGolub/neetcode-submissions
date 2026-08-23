8bb7d0ab5c8e7bf31654b3526ea7029e1ff14714
Mladen Golubovic <gm210544d@student.etf.bg.ac.rs>
2026-08-23 19:35:33 +0200

    Add: decode-string - submission-2

---
+++ Data Structures & Algorithms/decode-string/submission-2.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        
+        stack<string> stek;
+
+        string ret = "";
+
+        string pom = "";
+
+        string broj = "";
+
+        for(char c : s) {
+            if(c > 47 && c < 58) {
+                broj += c;
+            } else if(c == ']') {
+                while(stek.top() != "[") {
+                    pom = stek.top() + pom;
+                    stek.pop();
+                }
+                stek.pop();
+                int br = stoi(stek.top());
+                stek.pop();
+                string mnoz = pom;
+                while(br > 1) {
+                    pom = pom + mnoz;
+                    br--;
+                }
+                stek.push(pom);
+                pom = "";
+            } else {
+                if(broj != "") stek.push(broj);
+                broj = "";
+                string seljak = string(1,c);
+                stek.push(seljak);
+            }
+        }
+
+        while(!stek.empty()) {
+            ret = stek.top() + ret;
+            
+            stek.pop();
+        }
+
+        return ret;
+    }
+};
