4b7dba3896eeff89be312b960212427f9f204408
Mladen Golubovic <gm210544d@student.etf.bg.ac.rs>
2026-08-23 20:00:53 +0200

    Add: search-insert-position - submission-0

---
+++ Data Structures & Algorithms/search-insert-position/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        
+        int l = 0;
+        int r = nums.size() - 1;
+
+        if(target < nums[0]) return 0;
+        if(target > nums[nums.size()-1]) return nums.size();
+ 
+        while(l < r) {
+            if(nums[(l + r) / 2] < target) {
+                l = (l + r) / 2 + 1;
+            } else {
+                r = (l + r) / 2;
+            }
+        }
+
+        return r;
+
+    }
+};
