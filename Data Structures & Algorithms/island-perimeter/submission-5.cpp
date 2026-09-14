73488ccf6f7f30d39a5fbd9d9e5ad512245ce971
Mladen Golubovic <gm210544d@student.etf.bg.ac.rs>
2026-09-14 16:46:47 +0200

    Add: island-perimeter - submission-5

---
+++ Data Structures & Algorithms/island-perimeter/submission-5.cpp
@@ -0,0 +1,44 @@
+class Solution {
+
+    int backtrack(vector<vector<int>>& grid, int r, int c, int prev) {
+        if(r < 0 || c < 0 || r >= grid.size() || c >= grid[0].size()) {
+            if(prev) return 1;
+            else return 0;
+        } 
+        if(grid[r][c] == -1) {
+            return 0;
+        }
+        if(grid[r][c] == 0) {
+            return 1;
+        }
+
+        int res = 0;
+
+        int tmp = grid[r][c];
+        grid[r][c] = -1;
+
+        res += backtrack(grid, r-1, c, tmp);
+        res += backtrack(grid, r+1, c, tmp);
+        res += backtrack(grid, r, c-1, tmp);
+        res += backtrack(grid, r, c+1, tmp);
+
+        return res;
+    }
+
+
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        int res = 0;
+        for(int i = 0; i < grid.size(); i++) {
+            for(int j = 0; j < grid[0].size(); j++) {
+                if(grid[i][j] == 1) {
+                    return backtrack(grid, i, j, 1);
+                    cout << res << endl;
+                }
+            }
+        }
+
+        return res;
+
+    }
+};
